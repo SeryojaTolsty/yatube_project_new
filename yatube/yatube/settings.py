01b13d01e7dfc2a86e4f9b992d96ee78bcde20fb
Sergei Anisimov <seroja.92@mail.ru>
2022-12-29 02:16:44 +0300

    добавлены footer, header, index, base

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+# Добавлено: Искать шаблоны на уровне проекта
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
