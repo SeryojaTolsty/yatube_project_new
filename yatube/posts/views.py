1a03cf43b657404e0a39f971146d61d57717631e
Sergei Anisimov <seroja.92@mail.ru>
2022-12-29 01:09:27 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': 'Главная старница',
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'text': 'Группы'
+    }
+    return render(request, template, context)
