df6eb408b45c9b9e685ed87e6defcbc39a11136d
Sergei Anisimov <seroja.92@mail.ru>
2022-12-29 21:54:42 +0300

    добавлены моедли ORM, добавлены миграции, добавлен суперпользователь, добавлена регистрация постов от пользователя

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,38 @@
+'''
+# Ещё вариант модели
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+#           # поле символов максимальная длина - 400
+    start_at = models.DateTimeField(auto_now_add=True)
+#           # автоматическое добавление даты публикации
+    description = models.TextField()
+#           # обычное текстовое поле, можно без указания длины строки
+    contact = models.EmailField()
+#           # можно не указывать ничего в этом проекте,
+#           # но в целом - лучше указать
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="events")
+#           # вторичный ключ, соединяется с первичным по связи 'contact'
+    location = models.CharField(max_length=400)
+#           # поле символов максимальная длина - 400
+'''
+
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
